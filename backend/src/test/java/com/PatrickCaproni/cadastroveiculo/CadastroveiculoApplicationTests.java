69f156db147ad150cfecf193389ac729125a1bf9
PatrickCaproni <patrick.-m@hotmail.com>
2020-10-02 15:28:33 -0300

    projeto criado

---
+++ backend/src/test/java/com/PatrickCaproni/cadastroveiculo/CadastroveiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PatrickCaproni.cadastroveiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroveiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
